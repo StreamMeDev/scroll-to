08ffa7ba38e75dd5e6c2726646cef7d51301d283
Cameron <cameron.webley@gmail.com>
2016-08-22 16:47:43 -0500

    module scaffolding

---
+++ test/index.js
@@ -0,0 +1,9 @@
+/* global describe, it */
+var assert = require('assert');
+var pkg = require('../');
+
+describe('scroll-to', function () {
+	it('should ...', function () {
+		assert(pkg);
+	});
+});
